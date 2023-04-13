135c7a37fcad65940f9515607d58f001bd80ddc4
Prashant Mishra <Prshntmishra033@gmail.com>
2023-04-13 17:26:15 +0530

    baseline

---
+++ src/test/java/com/example/qrscan/QrscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
